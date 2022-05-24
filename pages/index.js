39ece8cbe0fd80445d96db61c3d682cb5e070e93
ERROR403 <46407695+ERROR4-0-3@users.noreply.github.com>
2022-05-24 15:53:38 +0200

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -26,7 +26,7 @@ export default function Home() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Forked by{'Eyad Faisal'}
+          Forked by{' Eyad Faisal'}
           
         </a>
       </footer>
